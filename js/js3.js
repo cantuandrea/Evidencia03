4f4cf8834c0e3f8759dd4c48794e3f1d36bf903e
cantuandrea <aaracelycantu@gmail.com>
2022-10-28 14:53:36 -0500

    Se agregó el ejercicio de numeros perfectos

---
+++ js/js3.js
@@ -0,0 +1,31 @@
+/* EJERCICIO DE NUMEROS PERFECTOS*/
+let numeros = 4;
+let avance = 1
+let divisores = [];
+let perfectos = [];
+let contador = 1;
+
+while (true) {
+    contador = 1;
+    while (contador < avance) {
+        if (avance%contador == 0) {
+            divisores.push(contador);
+        }
+        contador++;
+    }
+
+    let verificar = (divisores.reduce((a, b) => a + b, 0));
+
+    if (avance == verificar) {
+        perfectos.push(avance);
+    }
+
+    divisores = [];
+
+    if (perfectos.length == numeros) {
+        break
+    } else {
+        avance++;
+    }
+}
+console.log("La cantidad de perfectos es: ", numeros, "Los números perfectos son: ", perfectos);
