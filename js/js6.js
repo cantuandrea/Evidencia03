5106be28e69cc00a860c3ddf4bda5c0de8888be8
cantuandrea <aaracelycantu@gmail.com>
2022-10-28 15:23:07 -0500

    Se agregó el ejercicio de calculadora

---
+++ js/js6.js
@@ -0,0 +1,23 @@
+function calcular() {
+    let numero1 = parseInt(document.getElementById("num1").value);
+    let numero2 = parseInt(document.getElementById("num2").value);
+    let operacion = (document.getElementById("operacion").value);
+    let resultado = document.getElementById("resul");
+    switch (operacion) {
+        case "+":
+            resultado.value = (numero1+numero2);
+            break;
+        case "-":
+            resultado.value = (numero1-numero2);
+            break;
+        case "*":
+            resultado.value = (numero1*numero2);
+            break;
+        case "/":
+            resultado.value = (numero1/numero2);
+            break;
+        default:
+            resultado.value = ("Por favor escriba una entrada válida para este procedimiento.");
+            break;
+    }
+}
