8a4a459157d6f787e20075790ac10904d697a9bf
cantuandrea <aaracelycantu@gmail.com>
2022-10-28 15:08:26 -0500

    Se agregó ejercicio de pastillas

---
+++ js/js4.js
@@ -0,0 +1,11 @@
+let hora = 5;
+let frecuencia = 6;
+let cantidad = Math.floor(24/frecuencia);
+
+for(i = 1;i<=cantidad;i++){
+    if(hora>23){
+        hora = hora-24
+    }
+    console.log(`Toma ${i}: ${hora}`)
+    hora = hora+frecuencia
+}
