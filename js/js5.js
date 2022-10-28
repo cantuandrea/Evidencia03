dc3d957b1e9eb766b8d3094d2f8726a1494f6c8c
cantuandrea <aaracelycantu@gmail.com>
2022-10-28 15:15:10 -0500

    Se agregó el ejercicio de funciones

---
+++ js/js5.js
@@ -0,0 +1,35 @@
+function divisores(numero) {
+    let total=0;
+    for(let i=1; i<=numero;i++){
+        if (numero%i==0) {
+            total++;
+        }
+    }
+    return total;
+};
+
+const divisoresV2 = function(numero){
+    let total=0;
+    for(let i=1; i<=numero;i++){
+        if (numero%i==0) {
+            total++;
+        }
+    }
+    return total;
+};
+
+const divisoresV3 = (numero) => {
+    let total=0;
+    for(let i=1; i<=numero;i++){
+        if (numero%i==0) {
+            total++;
+        }
+    }
+    return total;
+};
+
+function calcular(){
+    let numero = parseInt(document.getElementById("num").value);
+    let resultado = document.getElementById("resul");
+    resultado.value = divisores(numero);
+}
